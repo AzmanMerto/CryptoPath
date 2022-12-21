293a10708dcdac3ae1050d799c20bdb80f5d4be5
NomoteteS <nomotetes.onetrue@icloud.com>
2022-12-21 16:05:54 +0200

    feat: Fetch Coin Data

---
+++ CryptoPath/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViewModel.swift
+//  CryptoPath
+//
+//  Created by NomoteteS on 21.12.2022.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    init() {
+        fetchCoinData()
+    }
+    
+    func fetchCoinData() {
+        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print("DEBUG: Error \(error.localizedDescription)")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("DEBUG: Response code \(response.statusCode)")
+                return
+            }
+            
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+            print("DEBUG: Data code \(dataAsString)")
+            
+            do {
+                let coins = try JSONDecoder().decode([Coin].self, from: data)
+                print("DEBUG: Coins \(coins)")
+            } catch let error = error {
+                print("DEBUG: Failed to decode with error: \(error)")
+            }
+            
+        }.resume()
+    }
+}
