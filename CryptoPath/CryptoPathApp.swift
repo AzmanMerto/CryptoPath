14c8361740937ffe57e92ae90c15ce0dc45388b4
NomoteteS <nomotetes.onetrue@icloud.com>
2022-12-21 12:23:16 +0200

    Initial Commit

---
+++ CryptoPath/CryptoPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPathApp.swift
+//  CryptoPath
+//
+//  Created by NomoteteS on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
